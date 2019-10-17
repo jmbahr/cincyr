d7177f5751e82708c82b55b3d7fec0656ba36a7e
Joe Bahr <JoeBahr@Joes-MBP-2.lan1>
2019-10-16 21:39:35 -0400

    clean up documentation for get_token and export

--- R/get_token.R
+++ R/get_token.R
@@ -1,7 +1,12 @@
-#' Bring in your Socrata API token.
+#' @title Get Socrata API Token
+#'
+#' @description Load the Socrata API token stored in the .Renviron file
 #'
 #' @param token_name The name attached to the Socrata API token in a .Renviron file.
+#'
 #' @return The Socrata API token stored in a .Renviron file.
+#'
+#' @export
 
 get_token <- function(token_name = "SOCRATA_API_TOKEN") {
   Sys.getenv(token_name)
